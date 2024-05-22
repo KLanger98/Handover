0fa0ff88854004b8c78b45c1f52ba5e898706fc0
KL <karl.w.langer@gmail.com>
2024-05-22 18:47:32 +0930

    Added some details to header

---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,18 @@
+import { Avatar, Title } from "@mantine/core";
+
+
+
+const UserProfile = () => {
+  return (
+    <>
+        <Title>Update Profile Image: </Title>
+        <Avatar>
+
+        </Avatar>
+    
+    </>
+
+);
+};
+
+export default UserProfile;
